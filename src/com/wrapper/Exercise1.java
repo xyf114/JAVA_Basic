54e79eb6bbefd47021478b95f8cbb46ed9788a08
xyf114 <ivanxie114@gmail.com>
2022-04-03 22:48:57 +0800

    wrapper learn

--- src/com/wrapper/Exercise1.java
+++ src/com/wrapper/Exercise1.java
@@ -2,6 +2,41 @@ package com.wrapper;
 
 public class Exercise1 {
     public static void main(String[] args) {
+        Integer integer = new Integer(1);
+        Integer integer1 = new Integer(1);
+
+        System.out.println(integer == integer1);
+
+/*
+*         public static Integer valueOf(int i) {
+            if (i >= Integer.IntegerCache.low && i <= Integer.IntegerCache.high)
+                return Integer.IntegerCache.cache[i + (-Integer.IntegerCache.low)];
+            return new Integer(i);
+        }
+* */
+
+        //未超过范围直接返回数值
+        Integer integer2 = 1;
+        Integer integer3 = 1;
+        System.out.println(integer2 == integer3);
+        //超过范围重新new
+        Integer integer4 = 128;
+        Integer integer5 = 128;
+        System.out.println(integer4 == integer5);
+
+        //虽然未超过范围但integer7为新创建对象
+        Integer integer6 = 127;
+        Integer integer7 = new Integer(127);
+        System.out.println(integer6 == integer7);
+
+        //出现基本数据类型则比较数值大小
+        int integer8 = 127;
+        Integer integer9 = new Integer(127);
+        System.out.println(integer8 == integer9);
+        //出现基本数据类型则比较数值大小
+        int integer10 = 128;
+        Integer integer11 = new Integer(128);
+        System.out.println(integer10 == integer11);
 
     }
 }
---
+++ src/com/wrapper/StringDemo.java
@@ -0,0 +1,28 @@
+package com.wrapper;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String string1 = "abc";
+        String string2 = "abc";//当方法区常量池中有相同字符串时直接使用原地址
+
+        System.out.println(string1 == string2);
+        System.out.println(string1.equals(string2));
+
+        String string3 = new String("abc");
+        String string4 = new String("abc");//不同对象在堆中开辟不同内存空间，但value数组指向相同字符串地址
+        System.out.println(string1 == string3);
+        System.out.println(string1.equals(string3));
+        System.out.println(string3 == string4);
+
+        System.out.println(string3 == string4.intern());
+        System.out.println(string4 == string4.intern());
+
+        String string5 = "abc";
+        String string6 = "hello";
+        String string7 = string5 + string6;//在堆中创建了新的对象，可追踪源码学习
+        String string8 = "abchello";
+        System.out.println(string7 == string8);
+
+    }
+
+}
