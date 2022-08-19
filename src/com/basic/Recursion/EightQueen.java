a2985459f4255296ad730a1aa3364a2354e84520
Ivanxie <Ivanxie114@gmail.com>
2022-08-19 17:16:32 +0800

    base learn

---
+++ src/com/basic/Recursion/EightQueen.java
@@ -0,0 +1,11 @@
+package com.basic.Recursion;
+
+public class EightQueen {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Queen{
+
+}
--- src/com/basic/Recursion/HanoiTower.java
+++ src/com/basic/Recursion/HanoiTower.java
@@ -1,10 +1,23 @@
 package com.basic.Recursion;
-
 import java.util.Scanner;
 
 public class HanoiTower {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        Tower tower = new Tower();
+        tower.move(input,'A','B','C');
+    }
+}
 
+class Tower{
+    public void move(int num,char A,char B,char C){
+        if (num == 1) {
+            System.out.println(A + "->" + C);
+        }else {
+            move(num - 1, A, C, B);
+            System.out.println(A + "->" + C);
+            move(num - 1, B, A, C);
+        }
     }
 }
